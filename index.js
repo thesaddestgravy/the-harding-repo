94011c5bf3272c57ea638d29dc2054ae99b8f3fa
Taylor Harding <thesaddestgravy@gmail.com>
2020-05-11 16:11:02 -0700

    formatted tip data in list for prototyping

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const subtotal = document.querySelector("#subtotal");
 const tip = document.querySelector("#tip");
 const ul = document.querySelector("ul");
 
+var numDeliveries = 0;
+
+
 form.addEventListener("submit", e => {
     e.preventDefault();
+    numDeliveries++;
+
+
+    var today = new Date();
+    var todayHours = today.getHours();
+    var amorpm;
+    if (todayHours - 12 > 0) {
+        todayHours %= 12;
+        amorpm = "pm";
+    }
+    else {
+        amorpm = "am";
+    }
+    var time = todayHours +  ":" + today.getMinutes() + amorpm;
+
+
     subtotal.value = subtotal.value.toUpperCase();
-    console.log(tip.value);
     const li = document.createElement("li");
-    li.textContent = tip.value;
+    const percentTip = Math.round(tip.value/subtotal.value * 100);
+    li.textContent = `${numDeliveries}: $${tip.value} tip of $${subtotal.value} (${percentTip}%) at ${time}`;
     ul.prepend(li);
+    subtotal.value = null;
+    tip.value = null;
 });
 
