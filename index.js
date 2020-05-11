49e4488a92cc73ffdba08e122f6997fb67fed179
Taylor Harding <thesaddestgravy@gmail.com>
2020-05-11 15:36:32 -0700

    added tip list

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const form = document.querySelector(".form-group");
 const subtotal = document.querySelector("#subtotal");
+const tip = document.querySelector("#tip");
+const ul = document.querySelector("ul");
 
 form.addEventListener("submit", e => {
     e.preventDefault();
     subtotal.value = subtotal.value.toUpperCase();
+    console.log(tip.value);
+    const li = document.createElement("li");
+    li.textContent = tip.value;
+    ul.prepend(li);
 });
+
